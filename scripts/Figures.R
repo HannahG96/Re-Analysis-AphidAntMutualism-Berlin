bb992f3b45f41e48c5f0dca957d4eb13ed6d8e31
HannahG96 <HannahG96@users.noreply.github.com>
2021-05-23 12:43:37 +0200

    export figures as pdf

--- scripts/Figures.R
+++ scripts/Figures.R
@@ -1,7 +1,7 @@
 # Figures for article
-library("ggsci")
+#library("ggsci")
 library(interactions)
-
+library("viridis")
 #####################################################
 ###VISUALIZE INTERACTION OF APHID DENSITY WITH SEALING
 # Interaction plot of partial residuals:
@@ -14,6 +14,14 @@ Aph<-lmer(sqrt(N_aphid.mm) ~ date_s + meanAnt.mean_s + Seal_500_s +
             (1|plantPop),
           data=Aphid_density)
 
+#open graphical device:
+#-->2 column width
+pdf(file="figures/aphid_density.pdf",         # File name
+    width = 7.2, height = 5, # Width and height in inches
+    bg = "white",          # Background color
+    colormodel = "cmyk" )   # Color model (cmyk is required for most publications)
+
+#create figure:
 interactions::interact_plot(Aph,
                             pred = date_s, 
                             modx = Seal_500_s,
@@ -34,6 +42,10 @@ interactions::interact_plot(Aph,
                                             "Mean %Sealing (= 25%)",
                                             "+ 1SD (= 42%)")
 )
+
+# close the graphical device:
+dev.off() 
+
 
 # Check out the sealing values for legend:
 paste(c(round(mean(Aphid_density$Seal_500)-
@@ -93,6 +105,13 @@ newd$predicted <- predict(reaction.binom,
                           newdata=newd,
                           type = "response",re.form = NA)
 
+#open graphical device:
+#-->1.5 column width
+pdf(file="figures/ant_aggressivity_predicted.pdf",         # File name
+    width = 5, height = 4, # Width and height in inches
+    bg = "white",          # Background color
+    colormodel = "cmyk" )   # Color model (cmyk is required for most publications)
+
 #Produce plot based on predicted data:
 
 ggplot(data = newd, mapping = aes(Seal_500, predicted)) +
@@ -107,10 +126,14 @@ ggplot(data = newd, mapping = aes(Seal_500, predicted)) +
         axis.title.y=element_text(size=10, face="bold", color="gray21"),
         axis.title.x=element_text(size=10, face="bold", color="gray21"),
         panel.grid.major = element_line(colour="lightgrey", linetype="dashed"), 
-        panel.grid.minor = element_blank() ) +
+        panel.grid.minor = element_blank(),
+        legend.position=c(0.85,0.2)) +
   xlab("% Sealing") +
   ylab("Probability of aggressive reaction")+
   ylim(0,1)
+
+# close the graphical device:
+dev.off() 
 
 ####################
 #Raw data:
@@ -122,6 +145,14 @@ plotAggressivity.date <- summaryBy(formula = reaction + ant ~ Seal_500 +
 # calculate proportion of aggressive reactions
 plotAggressivity.date$prop_reaction <- 
   plotAggressivity.date$reaction.sum / plotAggressivity.date$ant.sum 
+
+#open graphical device:
+#-->1.5 column width
+pdf(file="figures/ant_aggressivity_data.pdf",         # File name
+    width = 5, height = 4, # Width and height in inches
+    bg = "white",          # Background color
+    colormodel = "cmyk" )   # Color model (cmyk is required for most publications)
+
 
 ggplot(plotAggressivity.date, aes(x=Seal_500, y=prop_reaction))+
   theme_minimal()+
@@ -136,9 +167,14 @@ ggplot(plotAggressivity.date, aes(x=Seal_500, y=prop_reaction))+
         axis.title.y=element_text(size=10, face="bold", color="gray21"),
         axis.title.x=element_text(size=10, face="bold", color="gray21"),
         panel.grid.major = element_line(colour="lightgrey", linetype="dashed"), 
-        panel.grid.minor = element_blank() ) +
+        panel.grid.minor = element_blank(),
+        legend.position=c(0.85,0.2)) +
   xlab("% Sealing") +
   ylab("Probability of aggressive reaction")
+
+# close the graphical device:
+dev.off() 
+
 
 ############################################################################
 ###VISUALIZE ANT BEHAVIOUR (Appendix)
@@ -172,12 +208,23 @@ plotTasks$Behaviour<-factor(plotTasks$Behaviour, levels=c("other behaviour",
 
 
 
-pal_material("indigo")(30)
+#obtain color palette that is consistent with other plots
+library("scales")
+show_col(viridis_pal(option="mako", begin=0, end=1, direction=-1)(15))
+
+#open graphical device:
+#-->2 column width
+pdf(file="figures/ant_behaviour.pdf",         # File name
+    width = 7.2, height = 5, # Width and height in inches
+    bg = "white",          # Background color
+    colormodel = "cmyk" )   # Color model (cmyk is required for most publications)
+
+#create figure:
 ggplot(plotTasks, aes(fill=Behaviour, y=Time_prop, x=Seal_500))+ 
   theme_minimal()+
   geom_bar(position = "stack", stat="identity", width = 1.75, color="#90A4ADFF")+
-  scale_fill_manual(values=c("seashell3", "#19227EFF", "#E7EAF6FF",
-                             "#9FA7D9FF", "#273492FF"),
+  scale_fill_manual(values=c("gray80", "#37659EFF", "#8AD9B1FF",
+                            "#40B7ADFF", "#3E3367FF"),
                     breaks=c("other behaviour",
                              "antinteraction", "standing",
                              "moving", "tending"),
@@ -190,6 +237,9 @@ ggplot(plotTasks, aes(fill=Behaviour, y=Time_prop, x=Seal_500))+
         axis.title.x=element_text(size=10, face="bold", color="gray21"),
         panel.grid.major = element_line(colour="lightgrey", linetype="dashed"), 
         panel.grid.minor = element_blank())
+
+# close the graphical device:
+dev.off() 
 
 #############################################################################################
 ######VISUALIZE APHID NUMBER (Appendix)
@@ -208,14 +258,24 @@ Aphids$plot.simple<-factor(Aphids$plot.simple, levels=c("Ol-11","Ol-55", "Nh-04"
                                                         "Nl-200"))
 Aphids$plantPop<-paste(Aphids$plot.simple, Aphids$plant, sep=" ")
 
+#open graphical device:
+#-->2 column width
+pdf(file="figures/aphid_numbers.pdf",         # File name
+    width = 7.2, height = 5.5, # Width and height in inches
+    bg = "white",          # Background color
+    colormodel = "cmyk" )   # Color model (cmyk is required for most publications)
+
+#create figure:
+
 ggplot(Aphids, aes(x=date, fill=Seal_500)) +
   theme_minimal()+
   geom_point(aes(y=N_aphid,shape=plant), size=2, color="#90A4ADFF") +
-  geom_line(aes(y=N_aphid, group=plant), color="#90A4ADFF")+
+  geom_line(aes(y=N_aphid, group=plant, color=Seal_500)) + #, color="#90A4ADFF")+
   scale_shape_manual("Aphid colony",values=c(21, 24, 23, 22, 25),
                      breaks=c("Alpha", "Beta", "Ceta", "Delta", "Gamma"))+
-  scale_fill_material("indigo")+
-  labs(fill="% Sealing")+
+  scale_fill_viridis(option="mako", begin=0.1, end=0.9, direction=-1)+
+  scale_color_viridis(option="mako", begin=0.1, end=0.9, direction=-1)+
+  labs(fill="% Sealing", color="% Sealing")+
   ylab("Number of aphids")+
   xlab("Date")+
   theme(axis.text.x = element_text(angle = 45, size=7, vjust = 1, hjust=1),
@@ -228,12 +288,20 @@ ggplot(Aphids, aes(x=date, fill=Seal_500)) +
         strip.background = element_blank())+
   facet_wrap(~plot.simple)
 
+# close the graphical device:
+dev.off() 
 
 ######################################################################################
 #ant attendance (Probably not relevant)
 Ant_attendance$plot.simple<-factor(Ant_attendance$plot.simple, levels=c("Ol-11","Ol-55", "Nh-04", "Nh-05",
                                                                         "Om-02", "Nl-55", "Nh-10", "Oh-02",
                                                                         "Nl-200"))
+#open graphical device:
+#-->2 column width
+pdf(file="figures/ant_numbers.pdf",         # File name
+    width = 7.2, height = 5.5, # Width and height in inches
+    bg = "white",          # Background color
+    colormodel = "cmyk" )   # Color model (cmyk is required for most publications)
 
 ggplot(Ant_attendance, aes(x=date, fill=Seal_500)) +
   theme_minimal()+
@@ -241,8 +309,9 @@ ggplot(Ant_attendance, aes(x=date, fill=Seal_500)) +
   geom_line(aes(y=meanAnt.mean, group=plant), color="#90A4ADFF")+
   scale_shape_manual("Aphid colony",values=c(21, 24, 23, 22, 25),
                      breaks=c("Alpha", "Beta", "Ceta", "Delta", "Gamma"))+
-  scale_fill_material("indigo")+
-  labs(fill="% Sealing")+
+  scale_fill_viridis(option="mako", begin=0.1, end=0.9, direction=-1)+
+  scale_color_viridis(option="mako", begin=0.1, end=0.9, direction=-1)+
+  labs(fill="% Sealing", color="% Sealing")+
   ylab("Number of ants")+
   xlab("Date")+
   theme(axis.text.x = element_text(angle = 45, size=7, vjust = 1, hjust=1),
@@ -255,3 +324,7 @@ ggplot(Ant_attendance, aes(x=date, fill=Seal_500)) +
         strip.background = element_blank())+
   facet_wrap(~plot.simple)
 
+# close the graphical device:
+dev.off() 
+
+#OR: just a simple plot showing that ant and aphid numbers are positively correlated (??)
